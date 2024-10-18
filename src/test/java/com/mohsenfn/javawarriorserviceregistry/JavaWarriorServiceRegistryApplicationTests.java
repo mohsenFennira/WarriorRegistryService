c837ec8163291556a32ad46c85e8e51803ecbe8d
mohsenfennira <mohsen.fennira@esprit.tn>
2024-10-18 06:21:33 +0100

    first commit

---
+++ src/test/java/com/mohsenfn/javawarriorserviceregistry/JavaWarriorServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohsenfn.javawarriorserviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWarriorServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
